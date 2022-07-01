d050f2c7d8b81724ec44093c8c8d66862b6e512b
Anton Gavryukhin <ah21@mail.ru>
2022-07-01 22:55:30 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 5202;// сумма стоимости билета
+        int miles = price / 20;//количество полученых бонусов
+        System.out.println("Стоимость билета " + price);
+        System.out.println("Количество получаемых бонусов " + miles);
+    }
+
+}
